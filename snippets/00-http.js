f45975eb5e07181f96db6f06218ec8776ff02397
Jonas Bögle <Jonas@sandstorm-projects.de>
2018-01-09 23:03:56 +0100

    updated, added comments

--- snippets/00-http.js
+++ snippets/00-http.js
@@ -1,6 +1,11 @@
+// import modules
+var engine = require('engine');
+
+// define data that should be sent
 var sendData = JSON.stringify({ foo: 'bar' });
 
-sinusbot.http({
+// send request
+http({
     'method': 'POST',
     'url': 'http://example.com',
     'timeout': 6000,
@@ -11,19 +16,27 @@ sinusbot.http({
         'Content-Length': sendData.length
     }
 }, function (error, response) {
+    
+    // check if request was successfull
     if (response.statusCode != 200) {
         engine.log(error);
         return;
     }
+    
+    // parse JSON response
     var res;
     try {
         res = JSON.parse(response.data);
     } catch (err) {
         engine.log(err.message);
     }
+    
+    // check if parsing was successfull
     if (res === undefined) {
         engine.log("Error in JSON!");
         return;
     }
+    
+    // success!
     engine.log(res);
 });
