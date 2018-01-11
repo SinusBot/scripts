89f2e4f7d313c4e41df106cecc4f80be2974909a
Jonas Bögle <Jonas@sandstorm-projects.de>
2018-01-11 20:04:35 +0100

    added basic http example

---
+++ snippets/01-basic-http.js
@@ -0,0 +1,19 @@
+// import modules
+var engine = require('engine');
+
+// send request
+http({
+    'method': 'GET',
+    'url': 'http://example.com',
+    'timeout': 6000,
+}, function (error, response) {
+
+    // check if request was successfull
+    if (response.statusCode != 200) {
+        engine.log(error);
+        return;
+    }
+
+    // success!
+    engine.log(res);
+});
