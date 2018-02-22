03d32858cdb4f5e14d0a07ef918bade7ee218149
Max Schmitt <max.andreas.schmitt@googlemail.com>
2018-02-22 17:03:25 +0100

    Updated Chuck Norris script to the new engine

--- scripts/01-speech-recognition.js
+++ scripts/01-speech-recognition.js
@@ -1,6 +1,6 @@
 registerPlugin({
     name: 'Chuck Norris Speech API',
-    version: '1.0',
+    version: '1.1',
     backends: ["ts3"],
     description: 'This is a simple script that will write you Chuck Norris jokes when you say "joke"',
     author: 'maxibanki <max@schmitt.mx> & irgendwer <Jonas@sandstorm-projects.de>',
@@ -11,41 +11,51 @@ registerPlugin({
         options: ['Poke', 'Private message', 'Channel message', 'Say it']
     }],
     voiceCommands: ['joke']
-}, function(sinusbot, config) {
-    sinusbot.setARC(2);
-    sinusbot.on('speech', function(ev) {
+}, function (sinusbot, config) {
+    var engine = require('engine')
+    var event = require('event')
+    var audio = require('audio')
+    var backend = require('backend')
+
+    audio.setAudioReturnChannel(0x2);
+
+    event.on('speech', function (ev) {
         if (ev.text == 'joke') {
-            sinusbot.log('Writes a joke to ' + ev.clientId);
+            engine.log('Writes a joke to ' + ev.clientId);
             sinusbot.http({
                 "method": "GET",
                 "url": "http://api.icndb.com/jokes/random",
                 "timeout": 6000,
                 "headers": [{
                     "Content-Type": "application/json"
-                }, ]
-            }, function(error, response) {
+                },]
+            }, function (error, response) {
                 if (response.statusCode != 200) {
-                    sinusbot.log(error);
+                    engine.log(error);
                     return;
                 }
                 var joke = unescape(JSON.parse(response.data).value.joke).replace('&quot;', '\"');
-                sinusbot.log("received joke: " + joke);
-                if (config.msgtype == 0) {
-                    if (joke.length > 100) {
-                        console.log("over 100");
-                        var joke = joke.substring(0, 96) + '...';
-                    }
-                    sinusbot.poke(ev.clientId, joke);
-                } else if (config.msgtype == 1) {
-                    sinusbot.chatPrivate(ev.clientId, joke);
-                } else if (config.msgtype == 2) {
-                    sinusbot.chatChannel(joke);
-                } else {
-                    sinusbot.say(joke);
+                engine.log("received joke: " + joke);
+                switch (config.msgtype) {
+                    case 0:
+                        if (joke.length > 100) {
+                            engine.log("over 100");
+                            var joke = joke.substring(0, 96) + '...';
+                        }
+                        ev.client.poke(joke);
+                        break;
+                    case 1:
+                        ev.client.chat(joke);
+                    case 2:
+                        backend.getCurrentChannel().chat(joke);
+                        break;
+                    default:
+                        audio.say(joke);
+                        break;
                 }
             });
         } else {
-            sinusbot.log('Client ' + ev.clientId + ' just said ' + ev.text);
+            engine.log('Client ' + ev.client.name() + ' just said ' + ev.text);
         }
     });
 });
