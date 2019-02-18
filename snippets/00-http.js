b5968b13a678be62a1c2e140b6d0e8b5c36abee2
Jonas Bögle <Jonas@sandstorm-projects.de>
2019-02-18 23:19:34 +0100

    add requiredModules to http examples

--- snippets/00-http.js
+++ snippets/00-http.js
@@ -1,46 +1,54 @@
-// import modules
-var engine = require('engine');
-var http = require('http');
+registerPlugin({
+    name: 'Demo http json Script',
+    version: '1.0.0',
+    description: 'This example actually does nothing',
+    author: 'Author <author@example.com>',
+    requiredModules: ['http'], // <-- don't forget this!
+    vars: []
+}, function(_, config, meta) {
+    // import modules
+    var engine = require('engine');
+    var http = require('http');
 
-// define data that should be sent
-var sendData = JSON.stringify({ foo: 'bar' });
+    // define data that should be sent
+    var sendData = JSON.stringify({ foo: 'bar' });
 
-// send request
-http.simpleRequest({
-    'method': 'POST',
-    'url': 'https://example.com',
-    'timeout': 6000,
-    'maxSize': 1024 * 1024 * 5,
-    'body': sendData,
-    'headers': {
-        'Content-Type': 'application/json',
-        'Content-Length': sendData.length
-    }
-}, function (error, response) {
-    if (error) {
-        engine.log("Error: " + error);
-        return;
-    }
-    
-    if (response.statusCode != 200) {
-        engine.log("HTTP Error: " + response.status);
-        return;
-    }
-    
-    // parse JSON response
-    var res;
-    try {
-        res = JSON.parse(response.data.toString());
-    } catch (err) {
-        engine.log(err.message);
-    }
-    
-    // check if parsing was successfull
-    if (res === undefined) {
-        engine.log("Invalid JSON.");
-        return;
-    }
-    
-    // success!
-    engine.log(res);
+    // send request
+    http.simpleRequest({
+        'method': 'POST',
+        'url': 'https://example.com',
+        'timeout': 6000,
+        'body': sendData,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Content-Length': sendData.length
+        }
+    }, function (error, response) {
+        if (error) {
+            engine.log("Error: " + error);
+            return;
+        }
+        
+        if (response.statusCode != 200) {
+            engine.log("HTTP Error: " + response.status);
+            return;
+        }
+        
+        // parse JSON response
+        var res;
+        try {
+            res = JSON.parse(response.data.toString());
+        } catch (err) {
+            engine.log(err.message);
+        }
+        
+        // check if parsing was successfull
+        if (res === undefined) {
+            engine.log("Invalid JSON.");
+            return;
+        }
+        
+        // success!
+        engine.log(res);
+    });
 });
--- snippets/01-basic-http.js
+++ snippets/01-basic-http.js
@@ -1,23 +1,32 @@
-// import modules
-var engine = require('engine');
-var http = require('http');
+registerPlugin({
+    name: 'Demo http basic Script',
+    version: '1.0.0',
+    description: 'This example actually does nothing',
+    author: 'Author <author@example.com>',
+    requiredModules: ['http'], // <-- don't forget this!
+    vars: []
+}, function(_, config, meta) {
+    // import modules
+    var engine = require('engine');
+    var http = require('http');
 
-// send request
-http.simpleRequest({
-    'method': 'GET',
-    'url': 'https://example.com',
-    'timeout': 6000,
-}, function (error, response) {
-    if (error) {
-        engine.log("Error: " + error);
-        return;
-    }
-    
-    if (response.statusCode != 200) {
-        engine.log("HTTP Error: " + response.status);
-        return;
-    }
-    
-    // success!
-    engine.log("Response: " + response.data.toString());
+    // send request
+    http.simpleRequest({
+        'method': 'GET',
+        'url': 'https://example.com',
+        'timeout': 6000,
+    }, function (error, response) {
+        if (error) {
+            engine.log("Error: " + error);
+            return;
+        }
+        
+        if (response.statusCode != 200) {
+            engine.log("HTTP Error: " + response.status);
+            return;
+        }
+        
+        // success!
+        engine.log("Response: " + response.data.toString());
+    });
 });
