d3f3435bc92bbed9aeed6c6dea40e8451b684e65
Jonas Bögle <jonas@boegle.me>
2020-03-21 18:27:57 +0100

    also log OS

--- default-attached/sinusbot-commands.js
+++ default-attached/sinusbot-commands.js
@@ -111,7 +111,7 @@ registerPlugin({
     const media = require('media')
 
     engine.log(`Loaded ${meta.name} v${meta.version} by ${meta.author}.`)
-    engine.log(`SinusBot v${engine.version()}`)
+    engine.log(`SinusBot v${engine.version()} on ${engine.os()}`)
 
     if (config.disable) {
         engine.log('SinusBot commands are DISABLED.')
